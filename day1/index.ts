0819208afaaec492f28049ea45d2d350fa7972a0
Max Otsuka <maxwellotsuka@gmail.com>
2021-12-07 22:49:13 -0500

    add day1 part 2 solution

--- day1/index.ts
+++ day1/index.ts
@@ -5,11 +5,14 @@ import {openFile} from '../util';
 const INPUT_FILEPATH = path.resolve(__dirname, 'input.txt');
 
 export default async function main() {
-  const result = await countIncreases(openFile(INPUT_FILEPATH));
-  console.log(`Number of increases: ${result}`);
+  const singleResult = await countSingleIncreases(openFile(INPUT_FILEPATH));
+  console.log(`Number of increases with window size 1: ${singleResult}`);
+
+  const trioResult = await countTrioIncreases(openFile(INPUT_FILEPATH));
+  console.log(`Number of increases with window size 1: ${trioResult}`);
 }
 
-async function countIncreases(file: readline.Interface): Promise<number> {
+async function countSingleIncreases(file: readline.Interface): Promise<number> {
   let increasesCount = 0;
   let previousValue: number | undefined;
   for await (const line of file) {
@@ -17,6 +20,27 @@ async function countIncreases(file: readline.Interface): Promise<number> {
     if (previousValue && currentValue > previousValue) {
       ++increasesCount;
     }
+    previousValue = currentValue;
+  }
+  return increasesCount;
+}
+
+async function countTrioIncreases(file: readline.Interface): Promise<number> {
+  let increasesCount = 0;
+  let previousValue: number | undefined;
+  let twoPreviousValue: number | undefined;
+  let threePreviousValue: number | undefined;
+  for await (const line of file) {
+    const currentValue = parseInt(line, 10);
+    if (previousValue && twoPreviousValue && threePreviousValue) {
+      const currentSum = currentValue + previousValue + twoPreviousValue;
+      const previousSum = previousValue + twoPreviousValue + threePreviousValue;
+      if (currentSum > previousSum) {
+        ++increasesCount;
+      }
+    }
+    threePreviousValue = twoPreviousValue;
+    twoPreviousValue = previousValue;
     previousValue = currentValue;
   }
   return increasesCount;
