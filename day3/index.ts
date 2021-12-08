0ae66ea636c6a43c8c9aacd33abb4e921e087d00
Max Otsuka <maxwellotsuka@gmail.com>
2021-12-08 00:33:58 -0500

    add day3 part 2 solution, not yet working

--- day3/index.ts
+++ day3/index.ts
@@ -4,22 +4,36 @@ import {openFile} from '../util';
 
 const INPUT_FILEPATH = path.resolve(__dirname, 'input.txt');
 
-type DiagnosticReportColumn = {zeros: number; ones: number};
-type DiagnosticReport = DiagnosticReportColumn[];
-type DiagnosticSummary = {gamma: number; epsilon: number};
+type ColumnSummary = {zeros: number; ones: number};
+type PowerConsumption = {gamma: number; epsilon: number};
+type LifeSupportRating = {oxygenGen: number; co2Scrub: number};
+type DiagnosticReport = PowerConsumption & LifeSupportRating;
 
 export default async function main() {
-  const result = await buildDiagnosticSummary(openFile(INPUT_FILEPATH));
-  console.log(`Gamma rate: ${result.gamma}`);
-  console.log(`Epsilon rate: ${result.epsilon}`);
-  console.log(`Product: ${result.gamma * result.epsilon}`);
+  const diagnosticReport = await calculateDiagnosticReport(
+    openFile(INPUT_FILEPATH)
+  );
+  console.log(`Gamma rate: ${diagnosticReport.gamma}`);
+  console.log(`Epsilon rate: ${diagnosticReport.epsilon}`);
+  console.log(
+    `Power consumption: ${diagnosticReport.gamma * diagnosticReport.epsilon}`
+  );
+  console.log(`Oxygen generator rating: ${diagnosticReport.oxygenGen}`);
+  console.log(`CO2 scrubber rating: ${diagnosticReport.co2Scrub}`);
+  console.log(
+    `Life support rating: ${
+      diagnosticReport.oxygenGen * diagnosticReport.co2Scrub
+    }`
+  );
 }
 
-async function buildDiagnosticSummary(
+async function calculateDiagnosticReport(
   file: readline.Interface
-): Promise<DiagnosticSummary> {
-  let report: DiagnosticReport = [];
+): Promise<DiagnosticReport> {
+  const lines: string[] = [];
+  const report: ColumnSummary[] = [];
   for await (const line of file) {
+    lines.push(line);
     for (let i = 0; i < line.length; ++i) {
       if (report.length <= i) {
         report.push({zeros: 0, ones: 0});
@@ -37,10 +51,18 @@ async function buildDiagnosticSummary(
       }
     }
   }
-  return summarizeReport(report);
+
+  const oxygenGeneratorRating = calculateRatingValue(report, lines, true);
+  const co2ScrubberRating = calculateRatingValue(report, lines, false);
+
+  return {
+    ...calculatePowerConsumption(report),
+    oxygenGen: parseInt(oxygenGeneratorRating, 2),
+    co2Scrub: parseInt(co2ScrubberRating, 2),
+  };
 }
 
-function summarizeReport(report: DiagnosticReport): DiagnosticSummary {
+function calculatePowerConsumption(report: ColumnSummary[]): PowerConsumption {
   let gammaRate = '';
   let epsilonRate = '';
   for (const column of report) {
@@ -52,4 +74,35 @@ function summarizeReport(report: DiagnosticReport): DiagnosticSummary {
     gamma: parseInt(gammaRate, 2),
     epsilon: parseInt(epsilonRate, 2),
   };
+}
+
+/**
+ * Finds + returns the rating value (a binary number in string form) for a
+ * given set of lines. Defaults to searching using most-common digit, unless
+ * leastCommon is true.
+ */
+function calculateRatingValue(
+  columnSummaries: ColumnSummary[],
+  lines: string[],
+  leastCommon: boolean
+): string {
+  let candidates = lines;
+  for (let i = 0; i < columnSummaries.length; ++i) {
+    if (candidates.length === 1) {
+      return candidates[0];
+    }
+    const column = columnSummaries[i];
+    const hasMoreZerosThanOnes = column.zeros > column.ones;
+    // XOR these to figure out if we are searching for 0 or 1.
+    const targetDigit = hasMoreZerosThanOnes !== leastCommon ? '0' : '1';
+    candidates = candidates.filter(c => c.charAt(i) === targetDigit);
+  }
+  if (candidates.length !== 1) {
+    throw new Error(
+      `Could not find ${
+        leastCommon ? 'CO2 scrubber' : 'oxygen generator'
+      } rating value!`
+    );
+  }
+  return candidates[0];
 }
