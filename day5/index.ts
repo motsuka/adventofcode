6dc661c63f9018fea9267860929347bc67d6ad00
Max Otsuka <maxwellotsuka@gmail.com>
2021-12-08 02:02:16 -0500

    start day5 part 1 solution, not working yet

---
+++ day5/index.ts
@@ -0,0 +1,61 @@
+import * as path from 'path';
+import {nextTick} from 'process';
+import * as readline from 'readline';
+import {openFile} from '../util';
+
+const INPUT_FILEPATH = path.resolve(__dirname, 'input.txt');
+
+export default async function main() {
+  const result = await calculateVeryDangerousAreas(openFile(INPUT_FILEPATH));
+  console.log(`There are ${result.size} very dangerous areas`);
+}
+
+async function calculateVeryDangerousAreas(
+  file: readline.Interface
+): Promise<Set<string>> {
+  const areasWithOneVent: Set<string> = new Set();
+  const areasWithMultipleVents: Set<string> = new Set();
+
+  for await (const line of file) {
+    const [beginning, end] = line.split(' -> ');
+    const coordinatesCoveredByVent = calculateLineCoordinates(beginning, end);
+    for (const coordinate of coordinatesCoveredByVent) {
+      if (areasWithMultipleVents.has(coordinate)) {
+        // Do nothing
+      } else if (areasWithOneVent.has(coordinate)) {
+        areasWithOneVent.delete(coordinate);
+        areasWithMultipleVents.add(coordinate);
+      } else {
+        areasWithOneVent.add(coordinate);
+      }
+    }
+  }
+
+  return areasWithMultipleVents;
+}
+
+function calculateLineCoordinates(beginning: string, end: string): string[] {
+  const [startX, startY] = beginning
+    .split(',')
+    .map(coordinate => parseInt(coordinate, 10));
+  const [endX, endY] = end
+    .split(',')
+    .map(coordinate => parseInt(coordinate, 10));
+
+  let x = startX;
+  let y = startY;
+  // initialize with the first postion
+  let coordinates = [`${x},${y}`];
+  while (x !== endX || y !== endY) {
+    if (x !== endX) {
+      x = x < endX ? x + 1 : x - 1;
+    }
+    if (y !== endY) {
+      y = y < endY ? y + 1 : y - 1;
+    }
+    // move, then add the next position
+    coordinates.push(`${x},${y}`);
+  }
+
+  return coordinates;
+}
--- solutions.ts
+++ solutions.ts
@@ -1,7 +1,15 @@
 import day1 from './day1';
 import day2 from './day2';
 import day3 from './day3';
+import day5 from './day5';
 
 type Solution = () => Promise<void>;
 
-export default [/*day0=*/ null, day1, day2, day3] as Solution[];
+export default [
+  /*day0=*/ null,
+  day1,
+  day2,
+  day3,
+  /*day4=*/ null,
+  day5,
+] as Solution[];
