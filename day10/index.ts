27d4f1cac2b44c793a607815007737a66915fb68
Max Otsuka <maxwellotsuka@gmail.com>
2021-12-10 22:04:19 -0500

    add day10 part 2 solution

--- day10/index.ts
+++ day10/index.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import * as readline from 'readline';
-import { openFile } from '../util';
+import {openFile} from '../util';
 
 const INPUT_FILEPATH = path.resolve(__dirname, 'input.txt');
 const OPEN_TO_CLOSE_CHAR_MAP: Map<string, string> = new Map()
@@ -8,45 +8,68 @@ const OPEN_TO_CLOSE_CHAR_MAP: Map<string, string> = new Map()
   .set('[', ']')
   .set('{', '}')
   .set('<', '>');
-const CHAR_TO_SCORE_MAP: Map<string, number> = new Map()
+const OPEN_CHAR_SET = new Set(OPEN_TO_CLOSE_CHAR_MAP.keys());
+const CLOSE_CHAR_SET = new Set(OPEN_TO_CLOSE_CHAR_MAP.values());
+const SYNTAX_ERROR_SCORE_MAP: Map<string, number> = new Map()
   .set(')', 3)
   .set(']', 57)
   .set('}', 1197)
   .set('>', 25137);
+const AUTOCOMPLETE_SCORE_MAP: Map<string, number> = new Map()
+  .set(')', 1)
+  .set(']', 2)
+  .set('}', 3)
+  .set('>', 4);
 
+type Scores = {syntaxError: number; autocomplete: number};
 
 export default async function main() {
-  const result = await calculateScore(openFile(INPUT_FILEPATH));
-  console.log(`Score: ${result}`);
+  const scores = await calculateScores(openFile(INPUT_FILEPATH));
+  console.log(`Syntax error score: ${scores.syntaxError}`);
+  console.log(`Median autocomplete score: ${scores.autocomplete}`);
 }
 
-async function calculateScore(file: readline.Interface): Promise<number> {
-  let score = 0;
+async function calculateScores(file: readline.Interface): Promise<Scores> {
+  let syntaxErrorScore = 0;
+  let autocompleteScores: number[] = [];
   for await (const line of file) {
     let foundCorruption = false;
-    const stack: Array<string | undefined> = [];
-    for (let i = 0; !foundCorruption && i < line.length; ++i) {
-      const char = line[i];
-      switch (char) {
-        case '(':
-        case '[':
-        case '{':
-        case '<':
-          stack.push(OPEN_TO_CLOSE_CHAR_MAP.get(char));
-          break;
-        case ')':
-        case ']':
-        case '}':
-        case '>':
-          if (stack.pop() === char) {
-            break;
-          }
-        default:
+    const stack: string[] = [];
+    for (const char of line) {
+      if (OPEN_CHAR_SET.has(char)) {
+        stack.push(OPEN_TO_CLOSE_CHAR_MAP.get(char) || '');
+      } else if (CLOSE_CHAR_SET.has(char)) {
+        if (stack.pop() !== char) {
           foundCorruption = true;
-          score += CHAR_TO_SCORE_MAP.get(char) || 0;
+          syntaxErrorScore += SYNTAX_ERROR_SCORE_MAP.get(char) || 0;
+        }
+      } else {
+        throw new Error(`found unexpected character: ${char}`);
       }
+    }
 
+    if (!foundCorruption) {
+      autocompleteScores.push(calculateAutocompleteScore(stack));
     }
   }
+  return {
+    syntaxError: syntaxErrorScore,
+    autocomplete: findMedianScore(autocompleteScores),
+  };
+}
+
+function calculateAutocompleteScore(remainingStack: string[]): number {
+  let score = 0;
+  while (remainingStack.length > 0) {
+    const char = remainingStack.pop() || '';
+    score *= 5;
+    score += AUTOCOMPLETE_SCORE_MAP.get(char) || 0;
+  }
   return score;
+}
+
+function findMedianScore(scores: number[]): number {
+  return (
+    scores.sort((a, b) => a - b).at(Math.floor(scores.length / 2.0)) || Infinity
+  );
 }
