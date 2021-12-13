f1f7fee6d3b5a30dfe1d1993853143ea4ea6bc3e
Max Otsuka <maxwellotsuka@gmail.com>
2021-12-13 03:36:02 -0500

    add day12 part 2 solution

--- day12/index.ts
+++ day12/index.ts
@@ -7,13 +7,27 @@ const SMALL_ROOM = /^[a-z]+$/;
 
 // TODO: do we care if this bleeds betwen runs?
 let paths = new Map<string, Set<string>>();
+// now we're getting even more hacky with global config
+let maxVisitsToSmallRoom = 0;
 
 export default async function main() {
-  const numPaths = await calculateTotalPaths(openFile(INPUT_FILEPATH));
+  let numPaths = await calculateTotalPaths(
+    openFile(INPUT_FILEPATH),
+    /*allowBonusSmallCaveVisit=*/ false
+  );
   console.log(`found ${numPaths} paths from start to end`);
+
+  const numPathsWithBonus = await calculateTotalPaths(
+    openFile(INPUT_FILEPATH),
+    /*allowBonusSmallCaveVisit=*/ true
+  );
+  console.log(`found ${numPathsWithBonus} paths from start to end (+ bonus)`);
 }
 
-async function calculateTotalPaths(file: readline.Interface): Promise<number> {
+async function calculateTotalPaths(
+  file: readline.Interface,
+  allowBonusSmallCaveVisit: boolean
+): Promise<number> {
   for await (const line of file) {
     const [left, right] = line.split('-');
     if (paths.has(left)) {
@@ -28,20 +42,39 @@ async function calculateTotalPaths(file: readline.Interface): Promise<number> {
     }
   }
 
-  return calculateNumPathsFrom('start', new Set(['start']));
+  return calculateNumPathsFrom(
+    'start',
+    new Set('start'),
+    allowBonusSmallCaveVisit
+  );
 }
 
-function calculateNumPathsFrom(source: string, visited: Set<string>): number {
+function calculateNumPathsFrom(
+  source: string,
+  visited: Set<string>,
+  allowBonusSmallCaveVisit: boolean
+): number {
   let numPaths = 0;
   for (const nextRoom of paths.get(source) || []) {
     if (nextRoom === 'end') {
       numPaths += 1;
-    } else if (SMALL_ROOM.test(nextRoom) && visited.has(nextRoom)) {
+    } else if (nextRoom === 'start') {
       continue;
+    } else if (SMALL_ROOM.test(nextRoom) && visited.has(nextRoom)) {
+      if (allowBonusSmallCaveVisit) {
+        numPaths += calculateNumPathsFrom(
+          nextRoom,
+          new Set(visited).add(nextRoom),
+          /*allowBonusSmallCaveVisit=*/ false
+        );
+      } else {
+        continue;
+      }
     } else {
       numPaths += calculateNumPathsFrom(
         nextRoom,
-        new Set(visited).add(nextRoom)
+        new Set(visited).add(nextRoom),
+        allowBonusSmallCaveVisit
       );
     }
   }
