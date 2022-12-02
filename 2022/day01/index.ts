2355cff59b32ab84f8116105f645cd12808a257b
Max Otsuka <motsuka@users.noreply.github.com>
2022-12-02 08:12:55 +0000

    Fix up day one part one so it actually runs :p

--- 2022/day01/index.ts
+++ 2022/day01/index.ts
@@ -1,27 +1,27 @@
 import * as path from 'path';
-import * as readlime from 'readline';
+import * as readline from 'readline';
 import {openFile} from '../util';
 
 const INPUT_FILENAME = path.join(__dirname, 'input.txt');
 
 export default async function main() {
-  const heaviest = await findHeaviestLoad(openFile(INPUT_FILENAME));
+  const heaviest = await findHeaviest(openFile(INPUT_FILENAME));
   console.log(`The elf carrying the most is carrying ${heaviest} calories.`);
 }
 
-async function findHeaviest(): number {
-  let heaviestLoad = 0;
-  let currentLoad = 0;
+async function findHeaviest(file: readline.Interface): Promise<number> {
+  let heaviest = 0;
+  let current = 0;
   for await (const line of file) {
     const item = parseInt(line, 10);
-    if (item !== NaN) {
-      currentLoad += item;
+    if (!Number.isNaN(item)) {
+      current += item;
     } else {
-      if (currentLoad > heaviestLoad) {
-        heaviestLoad = currentLoad;
+      if (current > heaviest) {
+        heaviest = current;
       }
-      currentLoad = 0;
+      current = 0;
     }
   }
-  return heaviestIndex;
+  return heaviest;
 }
